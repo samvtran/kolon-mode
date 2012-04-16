7581ca74419c0d9fb5fd32ce48102a02949b0e87
Sam Tran <samvtran@gmail.com>
2012-04-15 20:18:27 -0500

    Fixed column 0 tabbing Added kolon-show-version command Added kolon-open-docs command Added customizable tab width Added menubar

--- kolon-mode.el
+++ kolon-mode.el
@@ -1,6 +1,24 @@
 (require 'font-lock)
+(require 'easymenu)
 
 (defvar kolon-mode-hook nil)
+
+;; Customizable things
+(defconst kolon-mode-version "0.1"
+  "The version of `kolon-mode'.")
+
+(defgroup kolon nil
+  "Major mode for the Xslate Kolon syntax."
+  :group 'languages)
+
+(defcustom kolon-indent-offset tab-width
+  "Indentation offset for `kolon-mode'."
+  :type 'integer
+  :group 'kolon)
+
+(defvar kolon-mode-map (make-keymap)
+  "Keymap for `kolon-mode'.")
+
 (defvar kolon-keywords "block\\|override\\|cascade\\|include\\|super\\|before\\|after\\|around\\|with")
 (defconst kolon-font-lock-keywords 
   (list
@@ -10,12 +28,6 @@
    ;;'("\\(<:\\)\\(\\(?:.\\|
    ;;\\)+?\\)\\(:>\\)"  
    ;; (<:)((?:.|)+?)(:>)
-
-   ;;'("\\(<?:\\)\\(\\(?:.\\|\\)+?\\)\\(\\(:>\\)\\|\\({\\)\\|\\(}\\)\\)"
-   ;; '("\\(<?:\\)\\(\\(?:.\\|\\)+?\\)\\(\\(:>\\)\\|\\({}?\\)\\|\\(}\\)\\|\\(;\\)\\)"
-   ;; 	 (1 font-lock-string-face t)
-   ;; 	 (2 font-lock-variable-name-face t)
-   ;; 	 (3 font-lock-string-face t))
    '("\\(<:\\)\\(\\(?:.\\|\\)+?\\)\\(:>\\)"
 	 (1 font-lock-string-face t)
 	 (2 font-lock-variable-name-face t)
@@ -33,8 +45,6 @@
    )
   "Expressions to font-lock in kolon-mode.")
 
-(defvar kolon-indent-offset 4
-  "*Indentation offset for kolon-mode'.")
 (defun kolon-indent-line ()
   "Indent current line for Kolon mode."
   (let (previous-indentation)
@@ -45,9 +55,7 @@
 		(indent-line-to previous-indentation)
 	  (indent-line-to (+ (current-indentation) kolon-indent-offset)))))
 
-;; Using this because otherwise you end up with a single tab when otherwise you'd be at column 0
-;; Note that this adds a tab after a line at column 0 anyway, but it stops doing it after that.
-;; Confused yet?
+;; Prevents indentation at bob, bol, whitespace-only lines, or lines at column 0
 (defun indent-newline ()
   "Newline and indents"
   (interactive)
@@ -62,6 +70,8 @@
 	;; ... delete the whitespace, then add another newline:
 	(kill-line 0)
 	(newline))
+
+   ((= 0 (current-indentation)) (newline))
    
    ;; Else (not on whitespace-only) insert a newline,
    ;; then add the appropriate indent:
@@ -69,12 +79,34 @@
 	  (indent-according-to-mode)))
   )
 
+;; Menubar
+(easy-menu-define kolon-mode-menu kolon-mode-map
+  "Menu for `kolon-mode'."
+  '("Kolon"
+	["Kolon Docs" kolon-open-docs]
+	["Version" kolon-show-version]
+	))
+
+;; Print version number
+(defun kolon-show-version ()
+  "Prints the version number of `kolon-mode'."
+  (interactive)
+  (message (concat "kolon-mode version " kolon-mode-version)))
+
+;; Show Kolon docs
+(defun kolon-open-docs ()
+  "Shows Kolon syntax docs in the browser."
+  (interactive)
+  (browse-url "http://search.cpan.org/dist/Text-Xslate/lib/Text/Xslate/Syntax/Kolon.pm"))
 
 (define-derived-mode kolon-mode html-mode
-;;  (setq font-lock-defaults '(kolon-font-lock-keywords))
   (font-lock-add-keywords nil kolon-font-lock-keywords)
   (make-local-variable 'kolon-indent-offset)
   (set (make-local-variable 'indent-line-function) 'kolon-indent-line)
+
+  ;; Keybindings
+  (define-key kolon-mode-map (kbd "C-c C-k") 'kolon-open-docs)
+
   (setq mode-name "Kolon"))
 
 (provide 'kolon-mode)
